efe2ccb9b6e94f5afbacae5de3082861a61b39b9
Luciano <luduena.saavedra@gmail.com>
2019-11-07 21:02:58 -0300

    creando base de datos automaticamente

--- conexion_bd.js
+++ conexion_bd.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
+var fs = require('fs');
 
 //Ejemplos de parámetros de conexión. Completá con los parámetros que correspondan según tu usuario.
 var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'obcc-40b',
+    password: '',
     database: 'acamica'
 });
 
@@ -17,4 +18,41 @@ connection.connect(function(err) {
    
     console.log('connected as id ' + connection.threadId);
   });
+
+
+var sql = `DROP DATABASE IF EXISTS acamica;
+CREATE DATABASE acamica;
+USE acamica;
+CREATE TABLE tarea (
+ idTarea INT PRIMARY KEY AUTO_INCREMENT,
+ descripcion VARCHAR(100) NOT NULL,
+ prioridad INT(1) NOT NULL,
+ estado TINYINT
+);
+INSERT INTO tarea
+ (descripcion, prioridad, estado)
+VALUES
+ ('Hacer las compras', 1, 1);
+INSERT INTO tarea
+ (descripcion, prioridad, estado)
+VALUES
+ ('Ir a Acamica', 1, 0);
+INSERT INTO tarea
+ (descripcion, prioridad)
+VALUES
+ ('Ir al gimnasio', 5);
+CREATE TABLE agenda (
+   idAgenda INT PRIMARY KEY AUTO_INCREMENT,
+   fecha DATE NOT NULL,
+   idTarea INT NOT NULL,
+   FOREIGN KEY(idTarea) REFERENCES tarea(idTarea)
+);`
+
+connection.query(sql, function(error, resultado) {
+  if(error) {
+    console.log(error)
+  } else {
+    res.send(JSON.stringify(resultado));
+  }
+})
 module.exports = connection;
